ad4aacdfa9683da03f3f32ca672954277c5e78ca
Michael Osakoh <macmive@gmail.com>
2019-06-17 13:18:07 +0100

    Added the play button

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from tkinter import *
 
 root = Tk()  # creates a window
 root.title("Music Player")
-root.iconbitmap(r'red-audio.ico')
-# *************** Centralise the window **********************
+root.iconbitmap(r'img/red-audio.ico')
+
+txt = Label(root, text='Music is playing')
+txt.pack()
+
+photo = PhotoImage(file='img/play.png')
+labelPhoto = Label(root, image=photo)
+labelPhoto.pack()
+
+
+# ********************************************* Centralise the window **************************************************
 window_height = 300
 window_width = 500
 # specifies width and height of window1
@@ -13,9 +22,9 @@ screen_height = root.winfo_screenheight()
 # specifies the co-ordinates for the screen
 x_cordinate = int((screen_width / 2) - (window_width / 2))
 y_cordinate = int((screen_height / 2) - (window_height / 2))
-root.configure(background='#6b778d')
+# root.configure(background='#6b778d') fg="red"
 root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
 root.resizable(False, False)  # This code helps to disable windows from resizing
-# *************** End of code **********************
+# ******************************************* End of centralise code ***************************************************
 
 root.mainloop()
